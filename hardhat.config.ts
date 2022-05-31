9dd31adaa1d22fa62cbf91eb28a8a004cea7edcb
Vladyslav-Bochok <vladbochok1@gmail.com>
2022-05-31 21:57:57 +0100

    Add hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,33 @@
+import '@nomiclabs/hardhat-solpp';
+import '@typechain/hardhat';
+
+export default {
+    zksolc: {
+        version: '0.1.0',
+        compilerSource: 'docker',
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            },
+            experimental: {
+                dockerImage: 'matterlabs/zksolc'
+            }
+        }
+    },
+    zkSyncDeploy: {
+        zkSyncNetwork: 'https://zksync2-testnet.zksync.dev',
+        ethNetwork: 'goerli'
+    },
+    solidity: {
+        version: '0.8.8'
+    },
+    paths: {
+        sources: './contracts'
+    },
+    networks: {
+        hardhat: {
+            zksync: true
+        }
+    }
+};
